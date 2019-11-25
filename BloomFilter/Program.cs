17e8a228fd984b17c8144ed470c8b91021f65156
Anessollah Ima <anessollah.ima@students.fhnw.ch>
2019-11-25 12:42:34 +0100

    Created Bloom-Filter Project for next pragramming task

---
+++ BloomFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader();
+            Console.ReadLine();
+        }
+
+        private static void PrintHeader()
+        {
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Diskrete Stochastik");
+            Console.WriteLine("Bloom-Filter");
+            Console.WriteLine("Crated by Anessollah Ima & Jonathan Bättig");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
